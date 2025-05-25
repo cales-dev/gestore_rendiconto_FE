377e6fdc60d4db4f4fc5ba6524cc98d52e4c00c6
cales-dev <c.alessio2003@gmail.com>
2025-05-25 19:55:51 +0200

    Added login page HTML

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl('', Validators.compose([Validators.required])),
+  });
 
+  showPassword:boolean=false;
+
+
+
+  submit(){
+
+  }
 }
---
+++ src/app/services/local/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { UserInfo } from '../../model/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private isLogged = new BehaviorSubject<boolean>(false);
+  private userInfo = new BehaviorSubject<UserInfo>({} as UserInfo);
+  /* Metodo per impostare la sessione dell'utente dopo verifica del cookie */
+  createSession(user:UserInfo){
+    this.isLogged.next(true);
+    this.userInfo.next(user);
+  }
+  
+  flushSession(){
+    this.isLogged.next(false);
+    this.userInfo.next({} as UserInfo);
+  }
+
+  /* Metodo che ritorna lo stato del login */
+  isLoggedIn(){
+    return this.isLogged.getValue();
+  }
+
+  getUser(){
+    return this.userInfo.getValue();
+  }
+}
