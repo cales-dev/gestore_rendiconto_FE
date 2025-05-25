ecdcca7d98e5679799614786f160a8fe672c8343
cales-dev <c.alessio2003@gmail.com>
2025-05-25 17:51:16 +0200

    Added app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path:'dashboard', component: DashboardComponent }, // Default route
+  { path: 'details', component: DetailsComponent},
+  { path: 'settings', component: SettingsComponent},  
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo:'dashboard', pathMatch:"full"},// Default route
+];
+
