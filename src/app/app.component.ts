4e9eee1a96203575d5346c058d56e3bb82399322
cales-dev <c.alessio2003@gmail.com>
2025-05-25 19:45:29 +0200

    Added checkLogin call to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './services/local/auth.service';
+import { LoginService } from './services/http/login.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'gestore_rendiconto';
+
+  constructor(private authService:AuthService, private loginService:LoginService, private router:Router){
+    this.loginService.checkLogin().subscribe({
+      next:(response:any)=>{
+        if(!response.logged){
+          this.router.navigate(['/login']);
+        }else{
+          this.authService.createSession(response.user);
+        }
+      },
+      error:()=>{
+        //TODO error managment
+      }
+    })  
+  }
+
+  
 }
